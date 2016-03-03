f0d98b458c56801796395f9de8e840aa1b76a212
Venkat Kotu <vkotu@users.noreply.github.com>
2016-03-03 14:10:08 -0800

    Create nestedScope.js

---
+++ nestedScope.js
@@ -0,0 +1,32 @@
+
+var foo = "bar";
+
+function bar(){
+    var foo = "baz";
+    function baz(foo) { //foo treated as a local vairable to baz
+        foo = "bam";
+//     debugger;
+        bam = "yay";// will be kept in global scope
+    }
+    baz();
+
+    for(i = 10 ; i < 12 ; i++) {//i will be kept in global
+        console.log(i);
+    }
+
+    for(var j = 2; j < 3 ; j++) {
+        console.log(j);
+    }
+
+
+}
+
+
+
+
+bar();
+console.log(foo);
+console.log(bam);
+console.log(i); // i global
+console.log(j);// ref error
+console.log(baz);//ref error
