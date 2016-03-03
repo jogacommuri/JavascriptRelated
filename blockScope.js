6f087d7fdce8337eb4b9aaf6b5a8f5e250870bb4
Venkat Kotu <vkotu@users.noreply.github.com>
2016-03-03 15:10:39 -0800

    Update blockScope.js

--- blockScope.js
+++ blockScope.js
@@ -1,3 +1,5 @@
+//let keyword ES6
+
 function foo(bar) {
     if(bar) { 
         let baz = bar;
@@ -11,3 +13,14 @@ function foo(bar) {
 }
 
 foo("bar");
+
+//let does not hoist , so need to keep all at the top
+
+//The below sytnax is rejected and is not in ES6
+function foo1(bar) {
+    let (baz = bar) {
+        console.log(baz); //"bar"
+    }
+    console.log(baz); //error
+}
+foo1("bar");
