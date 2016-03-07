42f671d910bfe79a6bb35d44c2e185febd811b29
Venkat Kotu <vkotu@users.noreply.github.com>
2016-03-07 11:59:29 -0800

    Create OO.js

---
+++ OO.js
@@ -0,0 +1,22 @@
+function Foo(who){
+    this.me = who;
+}
+Foo.prototype.identify = function() {
+    return "Iam " + this.me;
+};
+
+
+var a1 = new Foo("a1");
+
+var a2 = new Foo("a2");
+
+a2.speak = function() {
+    alert("Hello, " + this.identify() + ".");
+};
+
+a1.constructor === Foo;
+a1.constructor === a2.constructor;
+a1.__proto__ === Foo.prototype;
+a1.__proto__ === Object.getPrototypeOf(a1);
+a1.__proto__ === a2.__proto__;
+a2.__proto__ === a2.constructor.prototype;
