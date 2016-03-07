a9f437859bce51adc082503a77757e5b1b853997
Venkat Kotu <vkotu@users.noreply.github.com>
2016-03-07 12:17:42 -0800

    Create InheritanceOLOO.js

---
+++ InheritanceOLOO.js
@@ -0,0 +1,19 @@
+//OLOO: delegated objects
+
+var Foo = {
+    init: function(who) {
+        this.me = who;
+    },
+    identify: function() {
+        return "I am "  + this.me;
+    }
+};
+
+var Bar = Object.create(Foo);
+Bar.speak = function() {
+    alert("Hello, " + this.identify() + ".");
+}; 
+
+var b1 = Object.create(Bar);
+b1.init("b1");
+b1.speak();
